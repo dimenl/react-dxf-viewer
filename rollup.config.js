469e541e7c3a814fe7106408e770698ecb502fdc
Darshan Sithan <40591538+darsh0230@users.noreply.github.com>
2025-07-05 12:56:33 +0530

    Add Rollup build setup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      useTsconfigDeclarationDir: true
+    })
+  ],
+  external: ['react', 'react-dom']
+};
